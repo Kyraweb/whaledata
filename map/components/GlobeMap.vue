d441bc8bdf4170e802049868fab6d4ac8da14a6a
Kyra Web Studio <163783175+Kyraweb@users.noreply.github.com>
2026-03-13 11:28:21 -0400

    feat: globe projection + ocean map style

--- map/components/GlobeMap.vue
+++ map/components/GlobeMap.vue
@@ -278,11 +278,12 @@ function animateDash() {
 onMounted(() => {
   map = new maplibregl.Map({
     container: mapContainer.value,
-    style: `https://api.maptiler.com/maps/dataviz-dark/style.json?key=${MAPTILER_KEY}`,
+    style: `https://api.maptiler.com/maps/ocean/style.json?key=${MAPTILER_KEY}`,
     center: [0, 20],
     zoom: 1.8,
     attributionControl: false,
-    renderWorldCopies: false
+    renderWorldCopies: false,
+    projection: { type: 'globe' }
   })
 
   map.addControl(new maplibregl.NavigationControl({ showCompass: false }), 'top-right')
